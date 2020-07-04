1cdb2c2131fa094a66eb77cf2aef2f2e5d106190
philipahlberg <philipahlberg@gmail.com>
2020-07-04 16:42:07 +0200

    Remove rollup-plugin-terser

--- scripts/rollup.prod.js
+++ scripts/rollup.prod.js
@@ -1,33 +1,14 @@
 import resolve from 'rollup-plugin-node-resolve';
-import { terser } from 'rollup-plugin-terser';
 import typescript from 'rollup-plugin-typescript';
 
-export default [
-  {
-    input: './src/index.ts',
-    output: {
-      file: './dist/index.js',
-      format: 'es'
-    },
-    plugins: [
-      typescript(),
-      resolve()
-    ]
+export default {
+  input: './src/index.ts',
+  output: {
+    file: './dist/index.js',
+    format: 'es'
   },
-  {
-    input: './src/index.ts',
-    output: {
-      file: './dist/index.min.js',
-      format: 'es',
-      sourcemap: true
-    },
-    plugins: [
-      typescript(),
-      resolve(),
-      terser({
-        ecma: 8,
-        module: true
-      })
-    ]
-  }
-]
+  plugins: [
+    typescript(),
+    resolve()
+  ]
+}
